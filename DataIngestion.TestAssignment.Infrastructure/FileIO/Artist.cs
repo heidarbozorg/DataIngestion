3820627393a7eb8e4b524a43ed0f0dc8d60b76c7
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-26 12:26:22 +0430

    Complete Artist class in infrastructure.FileIO

--- DataIngestion.TestAssignment.Infrastructure/FileIO/Artist.cs
+++ DataIngestion.TestAssignment.Infrastructure/FileIO/Artist.cs
@@ -12,7 +12,20 @@ namespace DataIngestion.TestAssignment.Infrastructure.FileIO
 
         protected override Domain.Entities.Artist Parse(string[] splitedStr)
         {
-            return null;
+            if (splitedStr == null || splitedStr.Length != 6)
+                return null;
+
+            var rst = new Domain.Entities.Artist()
+            {
+                ExportDate = long.Parse(splitedStr[0]),
+                ArtistId = long.Parse(splitedStr[1]),
+                Name = splitedStr[2],
+                IsActualArtist = int.Parse(splitedStr[3]) == 1,
+                ViewUrl = splitedStr[4],
+                ArtistTypeId = int.Parse(splitedStr[5])
+            };
+
+            return rst;
         }
     }
 }
