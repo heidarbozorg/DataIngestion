09588a7304e38dd580a3e99608016934310df331
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-26 16:11:09 +0430

    Add Unzip section

--- Program.cs
+++ Program.cs
@@ -5,26 +5,48 @@ namespace DataIngestion.TestAssignment
 {
 	class Program
 	{
+		static string _rootAddress = Environment.CurrentDirectory + "\\";
+
 		static List<Domain.Entities.Artist> _artists;
 		static List<Domain.Entities.ArtistCollection> _artistCollections;
 		static List<Domain.Entities.CollectionMatch> _collectionMatches;
-		static List<Domain.Entities.Collection> _collections;		
+		static List<Domain.Entities.Collection> _collections;
+
+		static void Unzip()
+        {
+			Console.WriteLine("2- Unzip files");
+			var unzipFolder = _rootAddress + "Unzip\\";
+			var downloadFolder = _rootAddress + "Downloads\\";
+			var zipFiles = new string[] 
+			{ 
+				"artist.zip", 
+				"artist_collection.zip" ,
+				"collection.zip",
+				"collection_match.zip"
+			};
+
+			foreach (var z in zipFiles)
+			{
+				Console.WriteLine("\r --> Unzip " + z);
+				System.IO.Compression.ZipFile.ExtractToDirectory(downloadFolder + z,
+							unzipFolder, true);
+			}
+		}
 
 		static void ReadFiles()
         {
             Console.WriteLine("3- Read unzip files");
-			var rootAddress = Environment.CurrentDirectory + "\\";
             Console.WriteLine("\r --> Read artist file.");
-			_artists = new Infrastructure.FileIO.ArtistRepository(rootAddress + "Unzip\\artist").GetAll();
+			_artists = new Infrastructure.FileIO.ArtistRepository(_rootAddress + "Unzip\\artist").GetAll();
 
 			Console.WriteLine("\r --> Read artist_collection file.");
-			_artistCollections = new Infrastructure.FileIO.ArtistCollectionRepository(rootAddress + "Unzip\\artist_collection").GetAll();
+			_artistCollections = new Infrastructure.FileIO.ArtistCollectionRepository(_rootAddress + "Unzip\\artist_collection").GetAll();
 
 			Console.WriteLine("\r --> Read collection_match file.");
-			_collectionMatches = new Infrastructure.FileIO.CollectionMatchRepository(rootAddress + "Unzip\\collection_match").GetAll();
+			_collectionMatches = new Infrastructure.FileIO.CollectionMatchRepository(_rootAddress + "Unzip\\collection_match").GetAll();
 
 			Console.WriteLine("\r --> Read collection file.");
-			_collections = new Infrastructure.FileIO.CollectionRepository(rootAddress + "Unzip\\collection").GetAll();
+			_collections = new Infrastructure.FileIO.CollectionRepository(_rootAddress + "Unzip\\collection").GetAll();
         }
 
 		static void InsertIntoElasticSearch()
@@ -49,8 +71,8 @@ namespace DataIngestion.TestAssignment
 		{
 			// Todo: 
 			// 1- Download from goodle drive
-			// 2- Unzip files
 
+			Unzip();
 			ReadFiles();
 			InsertIntoElasticSearch();
 
