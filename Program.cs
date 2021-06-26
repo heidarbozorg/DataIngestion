e306ed0139949db751c36310edf90df532405886
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-26 11:37:56 +0430

    Create Infrastructure project

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace DataIngestion.TestAssignment
 	{
 		static void Main(string[] args)
 		{
+			// Todo: 
+			// 1- Download from goodle drive
+			// 2- Unzip files
+			// 3- Read unzip files
+			// 4- Insert into ElasticSearch
+
 			Console.WriteLine("Hello World!");
 		}
 	}
