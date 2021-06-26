8da917edf45dac320f5bc7c973825f5405c5c1c4
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-26 13:03:57 +0430

    Add CollectionRepository to Infrastructure

---
+++ DataIngestion.TestAssignment.Infrastructure/FileIO/CollectionRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DataIngestion.TestAssignment.Infrastructure.FileIO
+{
+    public class CollectionRepository : CsvFileRepository<Domain.Entities.Collection>
+    {
+        public CollectionRepository(string fileAddress) : base(fileAddress)
+        {
+        }
+
+        private bool GetBool(string str)
+        {
+            int temp;
+            int.TryParse(str, out temp);
+            return temp == 1;
+        }
+
+        protected override Domain.Entities.Collection Parse(string[] splitedStr)
+        {
+            if (splitedStr == null || splitedStr.Length != 18)
+                return null;
+
+            var rst = new Domain.Entities.Collection()
+            {
+                CollectionId = long.Parse(splitedStr[1]),
+
+                Name = splitedStr[2],
+                ViewUrl = splitedStr[7],
+                ArtworkUrl = splitedStr[8],
+                OriginalReleaseDate = Convert.ToDateTime(splitedStr[9]),
+                LabelStudio = splitedStr[11],
+                IsCompilation = GetBool(splitedStr[16]),
+            };
+
+            return rst;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,13 +8,23 @@ namespace DataIngestion.TestAssignment
 		static List<Domain.Entities.Artist> _artists;
 		static List<Domain.Entities.ArtistCollection> _artistCollections;
 		static List<Domain.Entities.CollectionMatch> _collectionMatches;
+		static List<Domain.Entities.Collection> _collections;
 
 		static void ReadFiles()
         {
+            Console.WriteLine("3- Read unzip files");
 			var rootAddress = Environment.CurrentDirectory + "\\";
+            Console.WriteLine("\r --> Read artist file.");
 			_artists = new Infrastructure.FileIO.ArtistRepository(rootAddress + "Unzip\\artist").GetAll();
+
+			Console.WriteLine("\r --> Read artist_collection file.");
 			_artistCollections = new Infrastructure.FileIO.ArtistCollectionRepository(rootAddress + "Unzip\\artist_collection").GetAll();
+
+			Console.WriteLine("\r --> Read collection_match file.");
 			_collectionMatches = new Infrastructure.FileIO.CollectionMatchRepository(rootAddress + "Unzip\\collection_match").GetAll();
+
+			Console.WriteLine("\r --> Read collection file.");
+			_collections = new Infrastructure.FileIO.CollectionRepository(rootAddress + "Unzip\\collection").GetAll();
         }
 
 		static void Main(string[] args)
@@ -27,7 +37,7 @@ namespace DataIngestion.TestAssignment
 
 			ReadFiles();
 
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Done!");
 		}
 	}
 }
