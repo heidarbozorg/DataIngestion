df28e2f1a4a777b0f556c8dd0e56d2df8984037e
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-26 11:34:04 +0430

    Add ICsvFiles interface to domain

---
+++ DataIngestion.TestAssignment.Domain/Repositories/ICsvFiles.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DataIngestion.TestAssignment.Domain.Repositories
+{
+    public interface ICsvFiles<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Read Csv file and convert it to List
+        /// </summary>
+        /// <returns></returns>
+        List<TEntity> GetAll();
+    }
+}
