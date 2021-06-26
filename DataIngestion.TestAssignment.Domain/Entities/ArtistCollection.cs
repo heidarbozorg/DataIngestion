a59624dcfb143aecda97780cd090b903d9c50996
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-26 11:09:31 +0430

    Add ArtistCollection entity to domain

---
+++ DataIngestion.TestAssignment.Domain/Entities/ArtistCollection.cs
@@ -0,0 +1,11 @@
+﻿namespace DataIngestion.TestAssignment.Domain.Entities
+{
+    public class ArtistCollection
+    {
+        public long ExportDate { get; set; }
+        public long ArtistId { get; set; }
+        public long CollectionId { get; set; }
+        public bool IsPrimaryArtist { get; set; }
+        public int RoleId { get; set; }
+    }
+}
