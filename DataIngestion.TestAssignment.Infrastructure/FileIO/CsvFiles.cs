ce1a285ecdb8c8d430856f3efac6b3304f0b1155
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-26 11:42:36 +0430

    Add CsvFiles class to Infrastructure

---
+++ DataIngestion.TestAssignment.Infrastructure/FileIO/CsvFiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataIngestion.TestAssignment.Infrastructure.FileIO
+{
+    public class CsvFiles<TEntity> : Domain.Repositories.ICsvFiles<TEntity> where TEntity : class
+    {
+        private readonly string _fileAddress;
+
+        protected CsvFiles(string fileAddress)
+        {
+            _fileAddress = fileAddress;
+        }
+
+        public List<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
