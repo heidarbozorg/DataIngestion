3640a35b1ef4cc928d183540d63ef717a60ebb1b
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-26 11:12:23 +0430

    Add CollectionMatch entity to domain

---
+++ DataIngestion.TestAssignment.Domain/Entities/CollectionMatch.cs
@@ -0,0 +1,11 @@
+﻿namespace DataIngestion.TestAssignment.Domain.Entities
+{
+    public class CollectionMatch
+    {
+        public long ExportDate { get; set; }
+        public long CollectionId { get; set; }
+        public string Upc { get; set; }
+        public string Grid { get; set; }
+        public int AmgAlbumId { get; set; }
+    }
+}
