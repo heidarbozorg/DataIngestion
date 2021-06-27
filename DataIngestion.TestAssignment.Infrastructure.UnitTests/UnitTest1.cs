a5f5604a6447391369e9222afd0c1beea9916e53
Saeed Heidarbozorg <heidarbozorg@gmail.com>
2021-06-27 11:13:15 +0430

    Add DataIngestion.TestAssignment.Infrastructure.UnitTests project

---
+++ DataIngestion.TestAssignment.Infrastructure.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DataIngestion.TestAssignment.Infrastructure.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
